52df16e6ae682f84b2dcd5884906ae0f63c5f748
sasha190276 <48203818+sasha190276@users.noreply.github.com>
2026-04-03 19:18:51 +0300

    Update install_remnawave.sh

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -2368,7 +2368,10 @@ case $OPTION in
         ;;
     4)
         load_selfsteal_templates_module
-
+        if [[  -d "/opt/remnawave" &&  -d "/opt/remnanode" ]]; then
+            echo -e "${COLOR_YELLOW}${LANG[NO_PANEL_NODE_INSTALLED]}${COLOR_RESET}"
+            exit 1
+        else
             show_template_source_options
             reading "${LANG[CHOOSE_TEMPLATE_OPTION]}" TEMPLATE_OPTION
             case $TEMPLATE_OPTION in
@@ -2398,6 +2401,9 @@ case $OPTION in
                     echo -e "${COLOR_YELLOW}${LANG[INVALID_TEMPLATE_CHOICE]}${COLOR_RESET}"
                     exit 1
                     ;;
+            esac
+        fi
+        ;;
 
     5)
         manage_custom_legiz
